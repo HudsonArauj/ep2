248f1dc4f4873f3bca523a2a1d8bac6a4efddf61
Hudson Monteiro <hudsonmonteiro2011!gmail.com>
2021-05-07 18:17:09 -0300

    criamos outra pasta e adicionamos outras condições no jogo

--- funcoes_paciencia.py
+++ funcoes_paciencia.py
@@ -44,33 +44,5 @@ def possui_movimentos_possiveis(baralho):
             possibilidades = True
     return possibilidades   
 
-print('Paiciencia acordeao')
-print('o estado atual do baralho é:')
-baralho = cria_baralho()
 
-while possui_movimentos_possiveis(baralho) != False:    
-    for i in range(len(baralho)):
-        print('{0}. {1}'.format(i+1, baralho[i]))
-    entrada = input('escolha um numero de 1 a {}: '.format(len(baralho)))
-    possibilidades = lista_movimentos_possiveis(baralho, int(entrada)-1)
-    if len(possibilidades) == 0:
-        print("A carta {} não pode ser movida".format(baralho[int(entrada)-1]))
-        entrada = input('escolha um numero de 1 a {}: '.format(len(baralho)))
-    elif len(possibilidades) == 2:
-        print("Escolha sobra qual carta você que empilhar o {}".format(baralho[int(entrada)-1]))
-        print('{0}. {1}'.format(1, baralho[int(entrada)-2]))
-        print('{0}. {1}'.format(2, baralho[int(entrada)-4]))
-        escolha2 = ''
-        while escolha2 != '1' and escolha2 != '2':
-            escolha2 = input('Digite o número de sua escolha (1 ou 2): ')
-            if escolha2 == '1':
-                baralho = empilha(baralho, int(entrada)-1, int(entrada)-2)
-            elif escolha2 == '2':
-                baralho = empilha(baralho, int(entrada)-1, int(entrada)-4)
-            else:
-                print('Escolha inválida!')
-    elif len(possibilidades) == 1:
-        baralho = empilha(baralho, int(entrada)-1, int(entrada)-2)
-
-    possui_movimentos_possiveis(baralho) == False
     
